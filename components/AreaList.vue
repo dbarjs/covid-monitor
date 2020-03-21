2185cfef8b40075562257bd4286ea6b90ac21388
Eduardo Barros <joseeduardobarros@gmail.com>
2020-03-20 22:29:23 -0300

    Add AreaList and AreaListItem components

---
+++ components/AreaList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="areas" class="flex flex-col">
+    <area-list-item
+      v-for="(area, index) in areas"
+      :key="index"
+      :areaData="area"
+    ></area-list-item>
+  </div>
+</template>
+
+<script>
+import AreaListItem from '~/components/AreaListItem.vue'
+export default {
+  components: {
+    AreaListItem
+  },
+  computed: {
+    areas() {
+      return this.$store.getters.getAreaList
+    }
+  }
+}
+</script>
+
+<style></style>
