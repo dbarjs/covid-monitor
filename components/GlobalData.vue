6229b3e3f33c5d380f206b867ff831b3378817e0
Eduardo Barros <joseeduardobarros@gmail.com>
2020-03-20 22:28:53 -0300

    Add GlobalData component

---
+++ components/GlobalData.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="globalData">
+    <h3>{{ globalData.displayName }}</h3>
+    <p>{{ globalData.totalConfirmed }} infectados</p>
+    <p>{{ globalData.totalDeaths }} mortes</p>
+    <p>{{ globalData.totalRecovered }} recuperados</p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    globalData() {
+      return this.$store.getters.getData
+    }
+  }
+}
+</script>
+
+<style></style>
