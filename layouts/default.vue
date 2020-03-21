1dc5f24fd958e47ffc37a98cf616a69a4d70947d
Eduardo Barros <joseeduardobarros@gmail.com>
2020-03-20 22:29:54 -0300

    Add middleware to fetch data from COVID API

--- layouts/default.vue
+++ layouts/default.vue
@@ -88,6 +88,7 @@ export default {
       rightDrawer: false,
       title: 'Vuetify.js'
     }
-  }
+  },
+  middleware: 'data-fetch'
 }
 </script>
---
+++ middleware/data-fetch.js
@@ -0,0 +1,5 @@
+export default function({ store }) {
+  const apiURL =
+    'https://www.bing.com/covid/data?fbclid=IwAR0H3Pf41agrGI7BFRj-YpSujrSnpXt7VRQ-4geg9BtsxCZPZGCQDFD4X8Y&IG=5478213FEBF343D89A5F43FAC6815CCD'
+  store.dispatch('fetchData', apiURL)
+}
