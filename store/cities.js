9246d2d70681ed674ef2abc39c788da4a9069227
Eduardo Barros <joseeduardobarros@gmail.com>
2020-05-18 21:06:10 -0300

    store: create cities

---
+++ store/cities.js
@@ -0,0 +1,41 @@
+import { firestoreAction } from 'vuexfire'
+import { citiesRef, database } from '~/firebase'
+
+export const data = () => ({
+  cities: []
+})
+
+export const getters = {}
+
+export const actions = {
+  bindCities: firestoreAction((context) => {
+    context.bindFirestoreRef('cities', citiesRef)
+  }),
+  updateCities: (context, all = false) => {
+    const lastDate = context.rootGetters['config/getLastDate'].cities
+    const cities = context.rootGetters.getCities(all ? false : lastDate)
+    const cityList = Object.keys(cities)
+    let counter = 0
+    function update(currentIndex = 0, limit = 500) {
+      counter = currentIndex
+      const batch = database.batch()
+      while (counter < currentIndex + limit && counter < cityList.length) {
+        const cityId = cityList[counter]
+        batch.set(citiesRef.doc(cityId), cities[cityId], { merge: true })
+        counter++
+      }
+      batch.commit().then(function() {
+        // eslint-disable-next-line
+        console.log(`write ${counter} records`)
+        if (counter < cityList.length) {
+          setTimeout(() => {
+            update(counter)
+          }, 500)
+        } else {
+          context.dispatch('config/updateStatus', citiesRef, { root: true })
+        }
+      })
+    }
+    update()
+  }
+}
