80b11caf956bac59dee8dba67266c6bb1c71f439
Eduardo Barros <joseeduardobarros@gmail.com>
2020-05-19 21:11:54 -0300

    component: create Info

---
+++ components/Info.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="text-left full-width text-center">
+    <v-container class="pb-0">
+      <h1 class="display-2 green--text font-weight-black pb-0">
+        Covid-19
+      </h1>
+      <p class="display-1 font-weight-medium mb-0">
+        Monitoramento de Dados do Brasil
+      </p>
+      <p class="caption grey--text text--lighten-1 font-mono mb-0">
+        Atualização: {{ updateDate }}
+      </p>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import { DateTime } from 'luxon'
+export default {
+  props: {
+    updated: {
+      default: false,
+      type: [Object, Boolean]
+    }
+  },
+  computed: {
+    updateDate() {
+      return DateTime.fromSeconds(this.updated.seconds).toLocaleString(
+        DateTime.DATETIME_SHORT
+      )
+    }
+  }
+}
+</script>
+
+<style></style>
