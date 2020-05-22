c726fa6de60b780b20b6c700d997807aa2b6aecd
Eduardo Barros <joseeduardobarros@gmail.com>
2020-05-21 21:39:11 -0300

    component: create CitySearchResultItem

---
+++ components/CitySearchResultItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-list-item v-if="city" link color="grey" @click="selectCity(city.cityId)">
+    <v-list-item-title>
+      {{ city.name }}
+    </v-list-item-title>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  props: {
+    city: {
+      default: false,
+      type: [Object, Boolean]
+    }
+  },
+  methods: {
+    selectCity(cityId) {
+      this.$store.commit('cities/ADD_SELECTED_CITY', cityId)
+    }
+  }
+}
+</script>
+
+<style></style>
