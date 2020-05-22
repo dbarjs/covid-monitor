01701c0d33c1139eb480c7a9cf38d6f44bc4b0fd
Eduardo Barros <joseeduardobarros@gmail.com>
2020-05-22 18:18:56 -0300

    component: add sparkline to CityResume

--- components/CityResume.vue
+++ components/CityResume.vue
@@ -1,9 +1,23 @@
 <template>
-  <v-card v-if="city">
+  <v-card v-if="city" flat>
     <v-card-title>{{ city.city }}</v-card-title>
     <v-card-subtitle>
-      Casos: {{ city.totalCases }} Mortes: {{ city.deaths }}
+      Casos: {{ city.totalCases | number }} <br />
+      Mortes: {{ city.deaths | number }}
     </v-card-subtitle>
+    <v-container>
+      <v-sparkline
+        auto-draw
+        line-width="2"
+        :value="newCases"
+        :gradient="['#f72047', '#ffd200', '#1feaea']"
+        smooth="8"
+        padding="16"
+      ></v-sparkline>
+      <p class="caption text-center grey--text text--lighten-1">
+        Evolução de novos casos por dia
+      </p>
+    </v-container>
     <v-card-actions>
       <v-spacer></v-spacer>
       <v-btn icon color="red" @click="removeCity">
@@ -14,6 +28,7 @@
 </template>
 
 <script>
+import { DateTime } from 'luxon'
 import { citiesRef } from '~/firebase'
 export default {
   props: {
@@ -31,6 +46,43 @@ export default {
       city: false
     }
   },
+  computed: {
+    entries() {
+      try {
+        const dates = this.getDates(
+          DateTime.fromISO('2020-02-25'),
+          DateTime.local()
+        )
+        const entries = Object.values(this.city.entries)
+        let lastEntry = {}
+        return dates.map((date) => {
+          const index = entries.findIndex((entry) => entry.date === date)
+          if (index > -1) {
+            lastEntry = entries[index]
+            return lastEntry
+          } else {
+            return {
+              ...lastEntry,
+              date,
+              newCases: 0,
+              newDeaths: 0
+            }
+          }
+        })
+      } catch {
+        return []
+      }
+    },
+    newCases() {
+      try {
+        // eslint-disable-next-line
+        if (this.city.city === 'Londrina/PR') console.log(this.entries)
+        return this.city ? this.entries.map((entry) => entry.newCases) : []
+      } catch {
+        return [1]
+      }
+    }
+  },
   watch: {
     cityId: {
       immediate: true,
@@ -43,7 +95,16 @@ export default {
   },
   methods: {
     removeCity() {
-      this.$store.commit('cities/REMOVE_SELECTED_CITY', this.city.cityId)
+      this.$store.commit('cities/REMOVE_SELECTED_CITY', this.cityId)
+    },
+    getDates(firstDay, today) {
+      let current = firstDay
+      const dates = []
+      while (current <= today) {
+        dates.push(current.plus({ days: 1 }).toISODate())
+        current = current.plus({ days: 1 })
+      }
+      return dates
     }
   }
 }
