8572bc21dfd5806bd4112a5ce53459f6b945007a
Eduardo Barros <joseeduardobarros@gmail.com>
2020-05-21 21:41:03 -0300

    storage: add mutations and store2 integration

--- store/cities.js
+++ store/cities.js
@@ -1,16 +1,43 @@
+import store from 'store2'
 import { firestoreAction } from 'vuexfire'
 import { citiesRef, database } from '~/firebase'
 
-export const data = () => ({
-  cities: []
+export const state = () => ({
+  cities: [],
+  selectedCities: []
 })
 
-export const getters = {}
+export const mutations = {
+  SET_SELECTED_CITIES: (state, selectedCities) =>
+    (state.selectedCities = Array.isArray(selectedCities)
+      ? selectedCities
+      : []),
+  ADD_SELECTED_CITY: (state, cityId) => {
+    if (!state.selectedCities.find((id) => id === cityId)) {
+      state.selectedCities.push(cityId)
+      store('selectedCities', state.selectedCities)
+      return true
+    }
+    return false
+  },
+  REMOVE_SELECTED_CITY: (state, cityId) => {
+    const index = state.selectedCities.findIndex((id) => id === cityId)
+    if (index) {
+      state.selectedCities.splice(index, 1)
+    }
+  }
+}
+
+export const getters = {
+  getSelectedCities: (state) => state.selectedCities
+}
 
 export const actions = {
   bindCities: firestoreAction((context) => {
     context.bindFirestoreRef('cities', citiesRef)
   }),
+  getSelectedCitiesFromStore: ({ commit }) =>
+    commit('SET_SELECTED_CITIES', store('selectedCities')),
   updateCities: (context, all = false) => {
     const lastDate = context.rootGetters['config/getLastDate'].cities
     const cities = context.rootGetters['fetch/getCities'](
