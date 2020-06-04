96f10c705ec2019a70df514fddc58c0461eccdf7
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-04 17:37:17 -0300

    Add CitySearch component

--- components/SelectedCities.vue
+++ components/SelectedCities.vue
@@ -1,6 +1,11 @@
 <template>
   <v-layout v-if="selectedCities">
     <v-container>
+      <header class="mb-2">
+        <h2 class="headline font-weight-bold">Casos do vírus por cidade</h2>
+        <p class="subtitle-1 grey--text text--darken-2">Casos confirmados</p>
+      </header>
+      <city-search></city-search>
       <v-row>
         <v-col
           v-for="(cityId, index) in selectedCities"
@@ -17,14 +22,17 @@
 </template>
 
 <script>
+import CitySearch from '~/components/CitySearch.vue'
 import CityResume from '~/components/CityResume.vue'
 export default {
   components: {
+    CitySearch,
     CityResume
   },
   computed: {
     selectedCities() {
-      return this.$store.getters['cities/getSelectedCities']
+      const arr = [...this.$store.getters['cities/getSelectedCities']]
+      return arr.reverse()
     }
   },
   created() {
