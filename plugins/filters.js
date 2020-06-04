e2d5e848733ceda2f09627100fbf8b3b69441290
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-04 17:39:09 -0300

    Remove filter conditional test

--- plugins/filters.js
+++ plugins/filters.js
@@ -3,4 +3,4 @@ import numeral from 'numeral'
 import 'numeral/locales/pt-br'
 numeral.locale('pt-br')
 
-Vue.filter('number', (value) => (value ? numeral(value).format('0,0') : ''))
+Vue.filter('number', (value) => numeral(value).format('0,0'))
